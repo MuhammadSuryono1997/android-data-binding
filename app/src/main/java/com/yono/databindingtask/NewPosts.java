5a398b51d3a80f6adf8a4b8631caaafd813406d7
Muhammad Suryono <msuryono0@gmail.com>
2020-08-12 09:21:45 +0700

    Fix Old and Add new Posts

--- app/src/main/java/com/yono/databindingtask/NewPosts.java
+++ app/src/main/java/com/yono/databindingtask/NewPosts.java
@@ -1,12 +1,15 @@
 package com.yono.databindingtask;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -24,14 +27,39 @@ public class NewPosts extends AppCompatActivity implements LifecycleOwner {
     RecyclerView recyclerView;
     PostAdapter postAdapter;
 
+    ProgressBar progressBar;
+    EditText editTextSearch;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_posts);
         recyclerView = findViewById(R.id.rvPosts);
+        progressBar = findViewById(R.id.pbLoadingPosts);
+        editTextSearch = findViewById(R.id.etSearchPost);
+
+        showLoading(true);
+
         viewModelsPosts = ViewModelProviders.of(this).get(MainViewModelsPosts.class);
         viewModelsPosts.getPostLiveData().observe(this, postListUpdateObserve);
+
+        editTextSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 
     Observer<ArrayList<PostResponse>> postListUpdateObserve = new Observer<ArrayList<PostResponse>>() {
@@ -42,7 +70,16 @@ public class NewPosts extends AppCompatActivity implements LifecycleOwner {
             postAdapter.setPostAdapter(NewPosts.this, postResponses);
             recyclerView.setLayoutManager(staggeredGridLayoutManager);
             recyclerView.setAdapter(postAdapter);
+            showLoading(false);
 
         }
     };
+
+    public void showLoading(boolean isLoading) {
+        if (isLoading) {
+            progressBar.setVisibility(View.VISIBLE);
+        } else {
+            progressBar.setVisibility(View.GONE);
+        }
+    }
 }
--- app/src/main/java/com/yono/databindingtask/api/adapter/PostAdapter.java
+++ app/src/main/java/com/yono/databindingtask/api/adapter/PostAdapter.java
@@ -2,27 +2,31 @@ package com.yono.databindingtask.api.adapter;
 
 import android.content.Context;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 
 import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.yono.databindingtask.R;
+import com.yono.databindingtask.api.models.Users;
 import com.yono.databindingtask.api.response.PostResponse;
 import com.yono.databindingtask.databinding.NewItemPostsBinding;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> implements Filterable {
     Context context;
     private ArrayList<PostResponse> postResponseArrayList = new ArrayList<>();
+    private ArrayList<PostResponse> postResponseArrayListFiltered;
 
     public void setPostAdapter(Context context, final ArrayList<PostResponse> postResponsesList){
         this.context = context;
         this.postResponseArrayList = postResponsesList;
+        this.postResponseArrayListFiltered = postResponsesList;
         notifyDataSetChanged();
     }
 
@@ -42,12 +46,43 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull PostAdapter.ViewHolder holder, int position) {
-        holder.binData(postResponseArrayList.get(position));
+        holder.binData(postResponseArrayListFiltered.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return postResponseArrayList.size();
+        return postResponseArrayListFiltered.size();
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                String charString = charSequence.toString();
+                if (charString.isEmpty()) {
+                    postResponseArrayListFiltered = postResponseArrayList;
+                } else {
+                    ArrayList<PostResponse> filteredList = new ArrayList<>();
+                    for (PostResponse post : postResponseArrayList) {
+                        if (post.getTitlePosts().toLowerCase().contains(charString.toLowerCase())) {
+                            filteredList.add(post);
+                        }
+                    }
+                    postResponseArrayListFiltered = filteredList;
+                }
+
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = postResponseArrayListFiltered;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                postResponseArrayListFiltered = (ArrayList<PostResponse>) filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
